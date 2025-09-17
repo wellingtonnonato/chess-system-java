c571d64acf3cccbe9fe9ba0c984ff5d41dc19552
Wellington Nonato <wellingtonnonato@gmail.com>
2025-09-17 07:20:35 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serrialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+}
